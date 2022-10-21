7f8d00495ee76a708b5b5cd6fbf60feac0cf66bd
mzwealth <olarewajuaniyikaye@gmail.com>
2022-10-21 08:38:40 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (digits2 = digit1 + 1; digits2 < 10; digits2++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
