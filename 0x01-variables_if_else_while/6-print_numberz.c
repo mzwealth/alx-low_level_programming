da3e55375d2b7c655c8ffbc4650a522179c9cc15
mzwealth <olarewajuaniyikaye@gmail.com>
2022-10-21 07:20:43 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digits numbers of base 1o starting from 0, only using putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		puchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
